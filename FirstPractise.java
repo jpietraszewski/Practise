90772f67961c2cd8d0597fd607cb6258f528638f
jpietraszewski <79626962+jpietraszewski@users.noreply.github.com>
2021-05-17 13:43:59 +0200

    Add files via upload

---
+++ FirstPractise.java
@@ -0,0 +1,6 @@
+public class FirstPractise {
+    public static void main(String[] args) {
+        System.out.println("Welcome on my site");
+        System.out.println("Czy się udało?");
+    }
+}
